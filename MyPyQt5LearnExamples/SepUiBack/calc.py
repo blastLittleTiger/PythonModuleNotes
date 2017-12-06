fdded155b91b53459ed976d086cdcba1cc0900fe
Jesse Ren <2247359268@qq.com>
2017-12-07 00:24:51 +0800

    界面和逻辑分离
    界面和逻辑分离

---
+++ MyPyQt5LearnExamples/SepUiBack/calc.py
@@ -0,0 +1,75 @@
+# -------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+# 
+# __title__ = 'calc'
+# __purpose__ =
+# __author__ = 'prayjourney'
+# __mtime__ = '2017/12/7'
+# __copyright__='(c) renjiaxin.jesse 2017'
+# __licence__ = 'prayjourney 2017'
+# 
+#                 ┏ ┓   ┏ ┓
+#              ┏━━┛ ┻━━━┛ ┻━━┓
+#              ┃    #
+#              ┃   ┳┛  ┗*━   ┃    蹉跎错，消磨过，最是光阴化浮沫。
+#              ┃      ┻      ┃
+#              ┗━━┓       ┏━━┛
+#                 ┃         ┗━━━━━━━━━━┓
+#                 ┃  神兽保佑           ┣━┓
+#                 ┃  永无BUG！         ┏┛
+#                 ┗┓━┓ ┏━━━━━━━━━┳━┓━┓┛
+#                  ┃━┫━┫         ┃━┫━┫
+#                  ┗━┻━┛         ┗━┻━┛
+#
+# ---------------------------------------------------------------
+# calc.py
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget
+
+from pyqt2nd.ui_calc import Ui_Calc
+
+
+# 方式一
+class MyCalc(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Calc()
+        self.ui.setupUi(self)
+
+    @pyqtSlot(int)
+    def on_inputSpinBox1_valueChanged(self, value):
+        self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox2.value()))
+
+    @pyqtSlot(int)
+    def on_inputSpinBox2_valueChanged(self, value):
+        self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox1.value()))
+
+
+# 方式二
+# class MyCalc2(QWidget, Ui_Calc):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.setupUi(self)
+#
+#     @pyqtSlot(int)
+#     def on_inputSpinBox1_valueChanged(self, value):
+#         self.outputWidget.setText(str(value + self.inputSpinBox2.value()))
+#
+#     @pyqtSlot(int)
+#     def on_inputSpinBox2_valueChanged(self, value):
+#         self.outputWidget.setText(str(value + self.inputSpinBox1.value()))
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    win = MyCalc()
+    # win = MyCalc2()
+    win.show()
+    sys.exit(app.exec_())
+
+
+# https://www.cnblogs.com/hhh5460/p/5390999.html
+# http://blog.csdn.net/mr_zing/article/details/46945011
