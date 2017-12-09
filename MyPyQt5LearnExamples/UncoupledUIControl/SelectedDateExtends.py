c85e8823747d546859ed58cdbd08a165ad4c1d6b
Jesse Ren <2247359268@qq.com>
2017-12-09 23:50:12 +0800

    更新了类继承的写法以及实现方式
    更新了类继承的写法以及实现方式

--- MyPyQt5LearnExamples/UncoupledUIControl/SelectedDateExtends.py
+++ MyPyQt5LearnExamples/UncoupledUIControl/SelectedDateExtends.py
@@ -1,27 +1,5 @@
-# -------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
-# 
-# __title__ = 'SelectedDateExtends'
-# __purpose__ =
-# __author__ = 'prayjourney'
-# __mtime__ = '2017/12/3'
-# __copyright__='(c) renjiaxin.jesse 2017'
-# __licence__ = 'prayjourney 2017'
-# 
-#                 ┏ ┓   ┏ ┓
-#              ┏━━┛ ┻━━━┛ ┻━━┓
-#              ┃    #
-#              ┃   ┳┛  ┗*━   ┃    蹉跎错，消磨过，最是光阴化浮沫。
-#              ┃      ┻      ┃
-#              ┗━━┓       ┏━━┛
-#                 ┃         ┗━━━━━━━━━━┓
-#                 ┃  神兽保佑           ┣━┓
-#                 ┃  永无BUG！         ┏┛
-#                 ┗┓━┓ ┏━━━━━━━━━┳━┓━┓┛
-#                  ┃━┫━┫         ┃━┫━┫
-#                  ┗━┻━┛         ┗━┻━┛
-#
-# ---------------------------------------------------------------
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QMainWindow
@@ -29,12 +7,14 @@ from PyQt5.QtWidgets import QMainWindow
 from mypyqt5.SelectedDate import Ui_MainWindow
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+# 继承的类
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-
+        # super().__init__(parent)
         self.setupUi(self)
-        # 为啥这样不行呢？
+
         self.cal_widget.clicked[QDate].connect(self.show_date)
 
     def show_date(self, date):
--- MyPyQt5LearnExamples/UncoupledUIControl/UncoupledUIControl_2.py
+++ MyPyQt5LearnExamples/UncoupledUIControl/UncoupledUIControl_2.py
@@ -1,38 +1,14 @@
-# -------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
-# 
-# __title__ = 'UncoupledUIControl'
-# __purpose__ =
-# __author__ = 'prayjourney'
-# __mtime__ = '2017/12/3'
-# __copyright__='(c) renjiaxin.jesse 2017'
-# __licence__ = 'prayjourney 2017'
-# 
-#                 ┏ ┓   ┏ ┓
-#              ┏━━┛ ┻━━━┛ ┻━━┓
-#              ┃    #
-#              ┃   ┳┛  ┗*━   ┃    蹉跎错，消磨过，最是光阴化浮沫。
-#              ┃      ┻      ┃
-#              ┗━━┓       ┏━━┛
-#                 ┃         ┗━━━━━━━━━━┓
-#                 ┃  神兽保佑           ┣━┓
-#                 ┃  永无BUG！         ┏┛
-#                 ┗┓━┓ ┏━━━━━━━━━┳━┓━┓┛
-#                  ┃━┫━┫         ┃━┫━┫
-#                  ┗━┻━┛         ┗━┻━┛
-#
-# ---------------------------------------------------------------
+
 import sys
-from PyQt5.QtCore import QDate
 from mypyqt5 import SelectedDateExtends
 from PyQt5 import QtCore, QtWidgets, QtGui
 
 #  使用类继承的方式，来分离控制和UI
+#  此处用来控制，完全将控制和UI分离了，原始的UI:SelectedDate,继承的UI:SelectedDateExtends,本文件为控制文件
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = SelectedDateExtends.MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    newwindow = SelectedDateExtends.MainWindow()  # 此处只需要新的类来完成出创建和显示即可
+    newwindow.show()
     sys.exit(app.exec_())
