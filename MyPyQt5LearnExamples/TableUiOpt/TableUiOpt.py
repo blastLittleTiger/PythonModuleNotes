1e23ff6608f568d6d339e1baf8b6c10e4b33fbfd
Jesse Ren <2247359268@qq.com>
2017-12-16 01:12:29 +0800

    更新了菜单的操作，添加了对话框
    更新了菜单的操作，添加了对话框

--- MyPyQt5LearnExamples/TableUiOpt/TableUiOpt.py
+++ MyPyQt5LearnExamples/TableUiOpt/TableUiOpt.py
@@ -2,7 +2,7 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QTableWidgetItem
-from pyqt2nd import TableUi
+from pyqt2nd import TableUi, InfoDlg
 
 
 class TableUiOpt(QtWidgets.QMainWindow, TableUi.Ui_MainWindow):
@@ -34,16 +34,67 @@ class TableUiOpt(QtWidgets.QMainWindow, TableUi.Ui_MainWindow):
             print(row2)
             self.tableWidget.removeRow(row2 - 1)  # 动态删除行的时候，需要减1
 
-    def edittablerow(self):  # 自己添加数据
+    # def edittablerow(self):  # 自己添加数据
+    #     rowindex = 0
+    #     columninex = 0
+    #     if self.tableWidget.selectedItems() is None:
+    #         rowindex = 0
+    #         columninex = 0
+    #     else:
+    #         rowindex = self.tableWidget.currentRow()
+    #         columninex = self.tableWidget.currentColumn()
+    #         self.tableWidget.setItem(rowindex, columninex, QTableWidgetItem("001"))
+
+    def edittablerow(self):  # 弹出dlg
+        dlg = MyDialog()
+        dlg.show()
+        dlg.exec_()
+
+
+class MyDialog(QtWidgets.QDialog, InfoDlg.Ui_Dialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.initdlg()
+        self.pushBtnCancle.clicked.connect(self.clearEdit)
+        self.pushBtnOkay.clicked.connect(self.insertTableRow)
+
+    def initdlg(self):
+        listgender = ("男", "女")
+        self.comboBoxAge.addItems(listgender)
+        # 也可以使用additem
+        # self.comboBoxAge.addItem("男")
+
+    def clearEdit(self):
+        self.lineEdNo.setText("")
+        self.lineEdName.setText("")
+        self.lineEdAge.setText("")
+        self.lineEdPhone.setText("")
+        self.textEdOtherinfo.setText("")
+
+    # 一次只能输入一个cell，如何依次输入多个cell?
+    def insertTableRow(self):
+        lineedno = self.lineEdNo.text().strip()
+        lineedname = self.lineEdName.text().strip()
+        lineedage = self.lineEdAge.text().strip()
+        comboboxage = self.comboBoxAge.currentText()
+        lineedphone = self.lineEdPhone.text().strip()
+        lineedotherinfo = self.textEdOtherinfo.text().strip()
+        if (lineedno == "" | lineedname == "" | lineedage == "" | comboboxage == "" | \
+                lineedphone == "" | lineedotherinfo):
+            return 0
         rowindex = 0
-        columninex = 0
-        if self.tableWidget.selectedItems() is None:
+        if tableuiopt.tableWidget.selectedItems() is None:
             rowindex = 0
-            columninex = 0
         else:
-            rowindex = self.tableWidget.currentRow()
-            columninex = self.tableWidget.currentColumn()
-            self.tableWidget.setItem(rowindex, columninex, QTableWidgetItem("001"))
+            rowindex = tableuiopt.tableWidget.currentRow()
+            print(rowindex)
+            tableuiopt.tableWidget.setItem(rowindex, 0, QTableWidgetItem(lineedno))
+            # tableuiopt.tableWidget.setItem(rowindex, 1, QTableWidgetItem(lineedname))
+            # tableuiopt.tableWidget.setItem(rowindex, 2, QTableWidgetItem("123"))
+            # tableuiopt.tableWidget.setItem(rowindex, 3, QTableWidgetItem("123"))
+            # tableuiopt.tableWidget.setItem(rowindex, 4, QTableWidgetItem("123"))
+            # tableuiopt.tableWidget.setItem(rowindex, 5, QTableWidgetItem("123"))
 
 
 if __name__ == "__main__":
