1843b5f7ab08ce1a9d66505cc710b7b9fbe4723f
Jesse Ren <2247359268@qq.com>
2017-12-20 01:02:17 +0800

    添加了使用资源文件优点的说明
    添加了使用资源文件优点的说明

--- MyPyQt5LearnExamples/FirstResource/FirstResource.py
+++ MyPyQt5LearnExamples/FirstResource/FirstResource.py
@@ -1,12 +1,16 @@
-from PyQt5 import QtWidgets, QtGui
+# -*- coding:utf-8 -*-
 import sys
-
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QWindow, QIcon, QPixmap, QPalette, QBrush
 from PyQt5.QtWidgets import QApplication
 from pyqt2nd import ResourceUse
 from pyqt2nd import res2_rc  # 需要导入，不然显示有问题
-
+"""
+使用Resource的一个好处就是，我们可以将资源如图片，编译成16进制的文件
+这样，在文件丢失或者我们运行的环境转移的情况之下，使用Resource仍然可以
+获取到我们所需要的资源，而不必太过于担忧资源丢失的风险
+"""
 
 class FirstResource(QtWidgets.QMainWindow, ResourceUse.Ui_MainWindow):
     def __init__(self, parent=None):
